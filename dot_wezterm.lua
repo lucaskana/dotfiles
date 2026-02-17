a224f4ba357af0c04ca6e887d55a87620213c331
a0099243 <lucas.kanadani@telefonica.com>
2026-02-17 18:38:48 -0300

    upate wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -19,24 +19,31 @@ config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 
 -- my coolnight colorscheme:
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
-
-config.window_decorations = "RESIZE"
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#011423",
+-- 	cursor_bg = "#47FF9C",
+-- 	cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
+--
+-- config.window_decorations = "RESIZE"
 
 -- ======================
 -- 🎨 Aparência
 -- ======================
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Gruvbox Light"
+-- config.color_scheme = "Gruvbox Dark"
+-- config.color_scheme = "Nord"
+-- config.color_scheme = "catppuccin-frappe"
+config.color_scheme = "GruvboxDark"
+
 config.window_background_opacity = 0.90
 config.text_background_opacity = 0.90
 
@@ -70,6 +77,8 @@ config.keys = {
 
 	-- Modo fullscreen
 	{ key = "f", mods = "LEADER", action = wezterm.action.ToggleFullScreen },
+	{ key = "Enter", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+	-- { key = "m", mods = "CTRL|SHIFT", action = wezterm.action.TogglePaneZoomState },
 }
 
 return config
