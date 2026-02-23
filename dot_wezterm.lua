dc3b42b029465273f2685c1f7a64dc75860d9163
a0099243 <lucas.kanadani@telefonica.com>
2026-02-23 14:23:51 -0300

    add fish

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -18,30 +18,6 @@ config.line_height = 1.0
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 
--- my coolnight colorscheme:
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	cursor_bg = "#47FF9C",
--- 	cursor_border = "#47FF9C",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
---
--- config.window_decorations = "RESIZE"
-
--- ======================
--- 🎨 Aparência
--- ======================
--- config.color_scheme = "Catppuccin Mocha"
--- config.color_scheme = "Catppuccin Macchiato"
--- config.color_scheme = "Gruvbox Light"
--- config.color_scheme = "Gruvbox Dark"
--- config.color_scheme = "Nord"
--- config.color_scheme = "catppuccin-frappe"
 config.color_scheme = "GruvboxDark"
 
 config.window_background_opacity = 0.90
@@ -50,8 +26,8 @@ config.text_background_opacity = 0.90
 -- ======================
 -- ⌨️ Atalhos de teclado
 -- ======================
--- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 3000 }
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 3000 }
+-- config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 3000 }
 -- config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 
 config.keys = {
