729839dc510867a3524e1d1048efb35bd5cf12d7
a0099243 <lucas.kanadani@telefonica.com>
2026-02-27 08:53:09 -0300

    add zoxide e atuin

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -32,8 +32,8 @@ config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 3000 }
 
 config.keys = {
 	-- Dividir panes
-	{ key = "h", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "v", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
 	-- Navegar entre panes
 	{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
